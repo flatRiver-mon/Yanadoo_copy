1ef402b8f4e5692afa1fabdfb371295e1092c5df
FlatRiver <kpk0416@gmail.com>
2022-03-25 23:04:44 +0900

    2차 메뉴 작업 완료  및 메인 슬라이더 작업중

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
 $(document).ready(function(){
+
+    $(".menu-wrap .menu-box > li").mouseover(function(){
+        $(".menu-wrap .menu-box > li > ul").addClass("active");
+        $(".menu-wrap > .nav-back-box").addClass("active");
+    })
+
+    $(".menu-wrap > .nav-back-box").mouseleave(function(){
+        $(".menu-wrap > .nav-back-box").removeClass("active");
+        $(".menu-wrap .menu-box > li > ul").removeClass("active");
+    })
     
+    $(".main-slider-wrap > .main-slider").slick({
+        
+    })
 })
